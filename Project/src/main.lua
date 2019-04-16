c36e4ede198133b9c6e8972a8a5f37c0cbda0a99
wangxuhe <1053066913@qq.com>
2019-04-16 14:49:06 +0800

    add vscode LuaDebug

--- Project/src/main.lua
+++ Project/src/main.lua
@@ -4,8 +4,20 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 require "config"
 require "cocos.init"
 
+--添加调试代码
+local breakInfoFun,xpcallFun = require("LuaDebug")("localhost", 7003)
+-- 添加断点定时器
+cc.Director:getInstance():getScheduler():scheduleScriptFunc(breakInfoFun, 0.5, false)
+
 local function main()
     require("app.MyApp"):create():run()
+end
+
+--2.程序异常监听            
+__G__TRACKBACK__ = function(msg)
+    debugXpCall()
+    local msg = debug.traceback(msg, 3)       
+    print(msg)                     
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
