4b92a726da06c8b168dbbe4278aa1cf91e5b11fd
wangxuhe <wangxuhe@lohogames.com>
2019-06-10 21:36:29 +0800

    添加工具-excel转化lua配置文件

---
+++ Project/tool/convertExcelLua/out/mall_GoldConfig.lua
@@ -0,0 +1,11 @@
+local mall_GoldConfig = {
+	[301] = {itemId = 301,itemName = "10000金币",count = 10000,price = 6,isEmail = true,},
+	[302] = {itemId = 302,itemName = "20000金币",count = 20000,price = 10,isEmail = true,},
+	[303] = {itemId = 303,itemName = "50000金币",count = 50000,price = 30,isEmail = true,},
+	[304] = {itemId = 304,itemName = "100000金币",count = 100000,price = 50,isEmail = true,},
+	[305] = {itemId = 305,itemName = "200000金币",count = 200000,price = 60,isEmail = true,},
+	[306] = {itemId = 306,itemName = "500000金币",count = 500000,price = 80,isEmail = true,},
+	[307] = {itemId = 307,itemName = "1000000金币",count = 1000000,price = 120,isEmail = true,},
+	[308] = {itemId = 308,itemName = "3000000金币",count = 3000000,price = 130,isEmail = true,},
+}
+return mall_GoldConfig
