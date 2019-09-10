0ba90c01981967630094eef6b60a0c70421d4642
wangxuhe <wangxuhe@lohogames.com>
2019-09-10 18:12:59 +0800

    修改定时器Demo

--- Project/src/app/Demo_Cocos/ScheduleTest.lua
+++ Project/src/app/Demo_Cocos/ScheduleTest.lua
@@ -24,23 +24,16 @@ function ScheduleTest:ctor()
         self:addChild(self._states[i])
     end 
 
-    local count1, count2 = 0, 0
-    local function timerUpdate(dt)
-        count1 = count1 + dt
-        self._states[1]:setString("定时刷新时间:" .. count1)
-    end 
-    
+    local count1, count2 = 0, 0    
     local function frameUpdate(dt)
         count2 = count2 + dt
         self._states[2]:setString("帧刷新时间:" .. count2)
     end
     
     --[[
-    定时器一： 帧刷新
+    定时器一： 帧刷新 （调用该方法一定为 Node 相关，且无法自定义时间间隔）
     参数1： 回调接口用于刷新
     参数2： 刷新优先级
-
-    调用该方法一定为 Node 相关
 
     其调用实质上就是C++中的 scheduleUpdate ， 该方法会在每帧调用C++中的 update 方法
     在 cocoslua中其 NodeEx.lua中 Node.scheduleUpdate 的实现就是此方法
@@ -49,22 +42,26 @@ function ScheduleTest:ctor()
     ]]
     self:scheduleUpdateWithPriorityLua(frameUpdate, 0)
 
+
+    local function timerUpdate(dt)
+        count1 = count1 + dt
+        self._states[1]:setString("定时刷新时间:" .. count1)
+    end 
     --[[
     定时器二： 定时刷新
     参数1： 回调接口用于刷新
     参数2： 每次刷新的时间间隔
-    参数3： 是否仅执行一次，false为无限次
-
-    针对于参数3，若设置为 true 的话，也就是 C++中的 scheduleOnce 
-    在代码中添加判定的缘故，主要是为了避免重复创建出现问题
+    参数3： 是否仅执行一次，false为无限次 （此参数若设置为 true 的话，也就是 C++中的 scheduleOnce ）
 
     在销毁的时候，一定要调用 unscheduleScriptEntry
     ]]
+    local _schedule = cc.Director:getInstance():getScheduler()
+    -- 添加判定是为了避免定时器的重复定义
     if self._timerScheduler ~= nil then 
-        cc.Director:getInstance():getScheduler():unscheduleScriptEntry(self._timerScheduler)
+        _schedule:unscheduleScriptEntry(self._timerScheduler)
         self._timerScheduler = nil 
     end 
-    self._timerScheduler = cc.Director:getInstance():getScheduler():scheduleScriptFunc(timerUpdate, 5, false)
+    self._timerScheduler = _schedule:scheduleScriptFunc(timerUpdate, 5, false)
 end 
 
 function ScheduleTest:_onRemoveEvent(sender, eventType)
